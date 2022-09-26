d9eda4065093cf9543fd46a69b5732fa902dc8ca
Lucas Fonseca <lucasrfon@gmail.com>
2022-09-26 16:59:21 -0300

    implementa teste de adição de votos e2e

--- front-end/cypress/e2e/votes.cy.js
+++ front-end/cypress/e2e/votes.cy.js
@@ -1,5 +1,3 @@
-import { faker } from '@faker-js/faker';
-
 beforeEach(async () => {
   await cy.request('POST', 'http://localhost:5000/e2e/preset', {});
 });
@@ -8,12 +6,8 @@ describe('testa voto positivo nas música', () => {
 
     it('adiciona voto com sucesso', () => {
         cy.visit('http://localhost:3000/');
-
-        const votesBefore = cy.get(Number).first();
         
         cy.get("[data-cy=upArrow]").first().click();
-        console.log(votesBefore)
-        return votesBefore
     });
 });
 
